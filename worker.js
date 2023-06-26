bb9d3dea43de86812a54b012b84d528bbea97899
sam-lippert <samuel@driv.ly>
2023-06-26 10:13:29 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -73,7 +73,7 @@ export class State {
       },
       instance,
       state: this.machineState,
-      events: this.serviceState?.nextEvents.map((e) => `${origin}/${instance}/${e.replace(' ', '+')}`),
+      events: this.serviceState?.nextEvents.map((e) => `${origin}/${instance}/${e.replaceAll(' ', '+')}`),
       user,
     }
     if (retval.events && !retval.events.length) delete retval.events
