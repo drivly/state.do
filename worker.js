82f129e14df676ddbb52a0d54345ce41c266882d
Samuel Lippert <lippertz@gmail.com>
2023-07-01 10:48:07 -0500

    #12 #13

--- worker.js
+++ worker.js
@@ -35,27 +35,47 @@ export class State {
       const callback = meta?.callback || state.configuration.flatMap((c) => c.config).reduce((acc, c) => ({ ...acc, ...c }), {}).callback
       if (callback) {
         const url = typeof callback === 'string' || callback instanceof String ? callback : callback.url
-        const init = callback.init || meta?.init || { }
+        const init = callback.init || meta?.init || {}
         init.method = meta?.method || init.method || 'POST'
         init.body = JSON.stringify(meta?.body || state.event)
         console.log({ url, init, state })
         const data = await fetch(url, init)
-        const event = this.serviceState?.nextEvents.find((e) => data.status.toString().match(new RegExp(e.replace(/[.*+?^${}()|[\]\\]/g, '\\$&').replace(/x/gi, '\\d'))))
+        const event = state?.nextEvents.find((e) => data.status.toString().match(new RegExp(e.replace(/[.*+?^${}()|[\]\\]/g, '\\$&').replace(/x/gi, '\\d'))))
         this.service.send(event || data.status.toString(), await data.json())
       }
     })
     this.service.start(state)
   }
 
+  async reset() {
+    // Stop the service and reset the state before restarting it
+    this.service.stop()
+    this.machineState = undefined
+    this.serviceState = undefined
+    await this.state.storage.delete('machineState')
+
+    // Restart the service
+    this.startMachine()
+  }
+
+  async update(machineDefinition) {
+    // Don't update if the new definition is empty or hasn't changed
+    if (!machineDefinition || machineDefinition === this.machineDefinition) return
+    this.service.stop()
+    await this.state.storage.put('machineDefinition', this.machineDefinition = machineDefinition)
+    this.startMachine(this.machineState)
+  }
+
   async fetch(req) {
     const { user, redirect, json, method, origin, pathSegments, search } = await this.env.CTX.fetch(req).then(res => res.json())
     if (redirect) return Response.redirect(redirect)
     const [instance, stateEvent] = pathSegments
-    if ((search || method === 'POST') && !this.machineDefinition) {
-      this.machineDefinition = (search && JSON.parse(decodeURIComponent(search.substring(1)))) || json
-      await this.state.storage.put('machineDefinition', this.machineDefinition)
-      this.startMachine()
-    }
+    const update = '?update='
+    const isSearchBasedUpdate = search.startsWith(update)
+    if ((search && (!this.machineDefinition || isSearchBasedUpdate) || method === 'POST'))
+      await this.update((search && JSON.parse(decodeURIComponent(search.substring(isSearchBasedUpdate ? update.length : 1)))) || json)
+    else if (search === '?reset')
+      await this.reset()
     if (stateEvent) this.service?.send(stateEvent)
 
     const retval = {
@@ -67,6 +87,8 @@ export class State {
         type: 'https://apis.do/state',
         endpoints: {
           create: origin + '/:key?{state_machine}',
+          reset: origin + '/:key?reset',
+          update: origin + '/:key?update={state_machine}',
           read: origin + '/:key',
           event: origin + '/:key/:event',
         },
